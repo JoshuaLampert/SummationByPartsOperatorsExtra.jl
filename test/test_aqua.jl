dd8382090e1f1a756306ca88c062ecab83eed4d8
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-05-06 16:04:17 +0200

    format

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -10,10 +10,10 @@
                                 # These are needed in the extensions, but not in the main module.
                                 # Since package extensions cannot have additional dependencies,
                                 # we need to load them in the main module.
-                                ignore = [:PreallocationTools,]),
-                    # We use type piracy for `mass_matrix_boundary`. This probably doesn't want to live
-                    # in SummationByPartsOperators.jl since it has some assumptions.
-                    piracies = false)
+                                ignore = [:PreallocationTools]),
+                  # We use type piracy for `mass_matrix_boundary`. This probably doesn't want to live
+                  # in SummationByPartsOperators.jl since it has some assumptions.
+                  piracies = false)
     @test isnothing(check_no_implicit_imports(SummationByPartsOperatorsExtra,
                                               skip = (Core, Base,
                                                       SummationByPartsOperatorsExtra.SummationByPartsOperators)))
