e556fc403168a582201a2664d70597d5917b3b01
Joshua Lampert <51029046+JoshuaLampert@users.noreply.github.com>
2025-06-02 14:24:22 +0200

    Generalize test for reproducing Radau weights with subcell operators (#20)

--- test/test_subcell_reproducing.jl
+++ test/test_subcell_reproducing.jl
@@ -11,10 +11,10 @@ end
     # Gauss-Radau nodes are defined on [-1, 1]
     a = -1.0
     b = 1.0
-    # Construct `D` on [-1, 1] with subcells [-1, 0] and [0, 1]
+    # Construct `D` on [-1, 1] with subcells [-1, x_M] and [x_M, 1]
     c = -1.0
-    d = 0.0
-    x_M = 0.0
+    d = 1.0
+    x_M = 0.1
     source = GlaubitzLampertNordströmWinters2025()
     for n in 2:6
         nodes_ref, weights = gaussradau(n)
@@ -22,15 +22,15 @@ end
         # Map the reference nodes to the left part and use symmetry
         # to construct the right part of the interval
         nodes = [
-            linear_map.(nodes_ref, a, b, c, d)...,
-            -reverse(linear_map.(nodes_ref, a, b, c, d))...
+            linear_map.(nodes_ref, a, b, c, x_M)...,
+            linear_map.(-reverse(nodes_ref), a, b, x_M, d)...
         ]
         D = subcell_operator(basis, nodes, x_M, source;
                              options = Optim.Options(; iterations = 10000,
                                                      g_tol = 1e-16))
 
-        # Multiply by 2 to account for the length of 1 for each subcell
-        # compared to 2 for the reference interval
-        @test all(2 * diag(mass_matrix(D)) .≈ [weights; reverse(weights)])
+        weights_D = diag(mass_matrix(D))
+        @test all((b - a) / (x_M - c) * weights_D[1:n] .≈ weights)
+        @test all((b - a) / (d - x_M) * weights_D[(n + 1):end] .≈ reverse(weights))
     end
 end
