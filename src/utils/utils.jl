89788b0afda7228d30aa31e99e920434338a1790
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-09-12 12:16:26 +0200

    rename boundary_left and _right to xmin and xmax

--- src/utils/utils.jl
+++ src/utils/utils.jl
@@ -1,6 +1,3 @@
-boundary_left(D::AbstractNonperiodicDerivativeOperator) = first(grid(D))
-boundary_right(D::AbstractNonperiodicDerivativeOperator) = last(grid(D))
-
 include("interpolation.jl")
 include("optimization.jl")
 include("sparsity_patterns.jl")
