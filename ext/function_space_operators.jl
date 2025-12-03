cdde8aa438ddf7a2fb7647a0e9f459c7dd741e2b
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-12-03 18:48:22 +0100

    forward kwargs

--- ext/function_space_operators.jl
+++ ext/function_space_operators.jl
@@ -1,36 +1,15 @@
 function SummationByPartsOperatorsExtra.function_space_operator(basis_functions,
                                                                 nodes::Vector{T},
                                                                 source::SourceOfCoefficients;
-                                                                basis_functions_weights = ones(T,
-                                                                                               length(basis_functions)),
-                                                                regularization_functions = nothing,
-                                                                min_real_eigen = 0.1,
                                                                 derivative_order = 1,
                                                                 accuracy_order = 0,
-                                                                bandwidth = length(nodes) -
-                                                                            1,
-                                                                size_boundary = 2 *
-                                                                                bandwidth,
-                                                                different_values = true,
-                                                                sparsity_pattern = nothing,
-                                                                autodiff = :forward,
-                                                                x0 = nothing,
-                                                                verbose = false,
-                                                                opt_alg = default_opt_alg(source),
-                                                                options = default_options(source,
-                                                                                          verbose)) where {
-                                                                                                           T,
-                                                                                                           SourceOfCoefficients
-                                                                                                           }
+                                                                kwargs...) where {T,
+                                                                                  SourceOfCoefficients
+                                                                                  }
     assert_first_derivative_order(derivative_order)
     sort!(nodes)
     weights, D = construct_function_space_operator(basis_functions, nodes, source;
-                                                   basis_functions_weights,
-                                                   regularization_functions,
-                                                   min_real_eigen,
-                                                   bandwidth, size_boundary,
-                                                   different_values, sparsity_pattern,
-                                                   opt_alg, options, autodiff, x0, verbose)
+                                                   kwargs...)
     return MatrixDerivativeOperator(first(nodes), last(nodes), nodes, weights, D,
                                     accuracy_order, source)
 end
